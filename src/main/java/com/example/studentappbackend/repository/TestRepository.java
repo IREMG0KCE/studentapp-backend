ee34919df518956e2f22cc4b25d215771771fbfe
İrem Gökçe <iremiskenderun@gmail.com>
2026-01-12 16:16:56 +0300

    yazar kitap commit

--- src/main/java/com/example/studentappbackend/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentappbackend.repository;
-
-import com.example.studentappbackend.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Student, Long> {
-}
